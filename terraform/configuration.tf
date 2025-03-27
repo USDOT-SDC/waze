f044a28951dc3f9511be1994db0cc745d6a6e1ea
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2025-03-27 19:23:23 -0400

    update version

--- terraform/configuration.tf
+++ terraform/configuration.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.92"
+      version = "~> 5.93"
     }
     archive = {
       source  = "hashicorp/archive"
