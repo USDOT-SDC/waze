58cb8ff3f8b6a4a7bfb3457057d395ebd48b66b2
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2025-04-01 13:05:34 -0400

    update logging

--- terraform/import.tf
+++ terraform/import.tf
@@ -4,6 +4,11 @@
 # }
 
 import {
+  to = module.ingest.aws_cloudwatch_log_group.this
+  id = "/aws/lambda/waze_ingest"
+}
+
+import {
   to = module.ingest_orchestrator.aws_cloudwatch_log_group.this
   id = "/aws/lambda/waze_ingest_orchestrator"
 }
