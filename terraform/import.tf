16c327df3f36bda09be6f914904457d29714e88a
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2025-04-04 22:00:37 -0400

    aws_appautoscaling imports are a pain!!!

--- terraform/import.tf
+++ terraform/import.tf
@@ -17,3 +17,62 @@ import {
   to = module.ingest2ddb.aws_cloudwatch_log_group.this
   id = "/aws/lambda/waze_ingest2ddb"
 }
+
+import {
+  to = module.ingest2ddb.aws_appautoscaling_target.alerts_gsi_read
+  id = "dynamodb/table/waze_ingest_alerts/index/utc_partition_index/dynamodb:index:ReadCapacityUnits"
+}
+import {
+  to = module.ingest2ddb.aws_appautoscaling_target.alerts_gsi_write
+  id = "dynamodb/table/waze_ingest_alerts/index/utc_partition_index/dynamodb:index:WriteCapacityUnits"
+}
+import {
+  to = module.ingest2ddb.aws_appautoscaling_target.irregularities_gsi_read
+  id = "dynamodb/table/waze_ingest_irregularities/index/utc_partition_index/dynamodb:index:ReadCapacityUnits"
+}
+import {
+  to = module.ingest2ddb.aws_appautoscaling_target.irregularities_gsi_write
+  id = "dynamodb/table/waze_ingest_irregularities/index/utc_partition_index/dynamodb:index:WriteCapacityUnits"
+}
+import {
+  to = module.ingest2ddb.aws_appautoscaling_target.jams_gsi_read
+  id = "dynamodb/table/waze_ingest_jams/index/utc_partition_index/dynamodb:index:ReadCapacityUnits"
+}
+import {
+  to = module.ingest2ddb.aws_appautoscaling_target.jams_gsi_write
+  id = "dynamodb/table/waze_ingest_jams/index/utc_partition_index/dynamodb:index:WriteCapacityUnits"
+}
+
+
+
+import {
+  to = module.ingest2ddb.aws_appautoscaling_policy.alerts_gsi_read
+  id = "dynamodb/table/waze_ingest_alerts/index/utc_partition_index/dynamodb:index:ReadCapacityUnits/waze_ingest_alerts-utc_partition_index-scaling-policy"
+}
+
+import {
+  to = module.ingest2ddb.aws_appautoscaling_policy.alerts_gsi_write
+  id = "dynamodb/table/waze_ingest_alerts/index/utc_partition_index/dynamodb:index:WriteCapacityUnits/waze_ingest_alerts-utc_partition_index-scaling-policy"
+}
+
+import {
+  to = module.ingest2ddb.aws_appautoscaling_policy.irregularities_gsi_read
+  id = "dynamodb/table/waze_ingest_irregularities/index/utc_partition_index/dynamodb:index:ReadCapacityUnits/waze_ingest_irregularities-utc_partition_index-scaling-policy"
+}
+
+import {
+  to = module.ingest2ddb.aws_appautoscaling_policy.irregularities_gsi_write
+  id = "dynamodb/table/waze_ingest_irregularities/index/utc_partition_index/dynamodb:index:WriteCapacityUnits/waze_ingest_irregularities-utc_partition_index-scaling-policy"
+}
+
+import {
+  to = module.ingest2ddb.aws_appautoscaling_policy.jams_gsi_read
+  id = "dynamodb/table/waze_ingest_jams/index/utc_partition_index/dynamodb:index:ReadCapacityUnits/waze_ingest_jams-utc_partition_index-scaling-policy"
+}
+
+import {
+  to = module.ingest2ddb.aws_appautoscaling_policy.jams_gsi_write
+  id = "dynamodb/table/waze_ingest_jams/index/utc_partition_index/dynamodb:index:WriteCapacityUnits/waze_ingest_jams-utc_partition_index-scaling-policy"
+}
+
+# <service-namespace>/<resource-id>/<scalable-dimension>/<policy-name>
