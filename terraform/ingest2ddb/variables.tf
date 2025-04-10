097ff5a174024f4aa8fe17b7850dd76d0b59d1a6
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2025-04-10 16:34:44 -0400

    add ddb tables

--- terraform/ingest2ddb/variables.tf
+++ terraform/ingest2ddb/variables.tf
@@ -3,6 +3,7 @@ variable "module_slug" {}
 variable "common" {}
 variable "partner_id" {}
 variable "orchestrator_lambda" {}
+variable "ddb_table" {}
 locals {
   common_tags = {
     "Module Slug" = var.module_slug
